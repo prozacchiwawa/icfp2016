dfcf5dcca4bf88cc0fee490a6062faf578734628
Adam Kelly <aqk@acm.org>
2016-08-05 19:36:06 -0700

    Add option to display latest snapshot.

--- query.py
+++ query.py
@@ -1,9 +1,20 @@
 import json
 from httpreq import do_http_get
+import sys
 
 url="http://2016sv.icfpcontest.org/api/snapshot/list"
 def do_query():
     return json.load(do_http_get(url))
 
 if __name__ == '__main__':
-    print do_query()
+    objs = do_query()
+    if 'latest' in sys.argv[1:]:
+       latest_time = 0
+       latest_obj = None
+       for obj in objs["snapshots"]:
+           if obj["snapshot_time"] > latest_time:
+               latest_time = obj["snapshot_time"]
+               latest_obj = obj
+       print latest_obj
+    else:
+        print objs
