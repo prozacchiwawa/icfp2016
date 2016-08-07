49e973d2a11ccb3e084ff9c2bbef074697293d3b
prozacchiwawa <art.yerkes@gmail.com>
2016-08-06 20:17:23 -0700

    Satisfies tells whether the partial solution satisfies the target area

--- area.py
+++ area.py
@@ -5,27 +5,29 @@ class ACoef(list):
     def __init__(self,vals,target):
         super(ACoef,self).__init__(self)
         self.extend([0 for v in vals])
-        self.caps = [ceil(target/v) for v in vals]
+        self.caps = [floor(target/v) for v in vals]
+        print self.caps
         
     def incr(self):
         carry = 1
         n = 0
-        while carry:
-            self[n%len(self)] += 1
-            if self[n%len(self)] >= self.caps[n%len(self)]:
-                self[n%len(self)] = 0
+        l = len(self)
+        while carry and n < l:
+            self[n] += 1
+            if self[n] > self.caps[n]:
+                self[n] = 0
                 carry = 1
                 n += 1
             else:
-                carry = 0
+                break
+        return n < l
 
 class AreaSolver:
     def evalValues(self,m,adv):
         return sum([m * a for (m,a) in zip(m,adv)])
 
     def isSolution(self,m,adv):
-        mod = (self.evalValues(self.areas[1:],self.avect[1:]) - self.target) / self.areas[0]
-        return Fraction(mod).denominator == 1
+        return (self.evalValues(self.areas,self.avect) - self.target) == 0
 
     def __init__(self,areas,target):
         self.areas = sorted(areas, key=lambda a: -a)
@@ -40,10 +42,18 @@ class AreaSolver:
 
         self.avect = ACoef(self.areas,target)
 
-        while not self.isSolution(self.areas,self.avect):
-            self.avect.incr()
+        self.good = True
+        while not self.isSolution(self.areas,self.avect) and self.good:
+            self.good = self.avect.incr()
 
         self.gcdmatrix = [[0]*len(self.areas) for _ in range(len(self.areas))]
         for i in range(len(self.areas)):
             for j in range(len(self.areas)):
                 self.gcdmatrix[i][j] = gcd(self.areas[i],self.areas[j])
+
+    def satisfies(self,copies):
+        val = self.target - sum([c * a for (c,a) in copies])
+        print val
+        if val < 0:
+            return False
+        return AreaSolver(self.areas,val).good
